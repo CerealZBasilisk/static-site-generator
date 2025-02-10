0f6e0eb7d88b5a2880d684cfbeae6d11a908f112
CerealZBasilisk <93325934+CerealZBasilisk@users.noreply.github.com>
2025-02-10 17:46:36 -0500

    added unittests

---
+++ src/test_textnode.py
@@ -0,0 +1,37 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.Bold)
+        node2 = TextNode("This is a text node", TextType.Bold)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.Bold)
+        node2 = TextNode("This is a text node link", 
+                         TextType.Links, 
+                         "https://www.test_url.com")
+        self.assertNotEqual(node, node2)
+
+    def test_link_eq(self):
+        node = TextNode("This is a text node link", 
+                        TextType.Links, 
+                        "https://www.test_url.com")
+        node2 = TextNode("This is a text node link", 
+                         TextType.Links, 
+                         "https://www.test_url.com")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a test", TextType.Bold, url="http://example.com")
+        
+        expected_repr = "TextNode(This is a test, bold, http://example.com)"
+        self.assertEqual(repr(node), expected_repr)
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
